fd14e490155087fa425a376307ce2d56633906b7
AMD <86533019@qq.com>
2016-07-14 22:08:38 +0800

    接口更新~

--- src/main/java/com/finefocus/tryspread/service/impl/GetTaskServiceImpl.java
+++ src/main/java/com/finefocus/tryspread/service/impl/GetTaskServiceImpl.java
@@ -10,6 +10,7 @@ import com.finefocus.tryspread.model.CodeAndMsg;
 import com.finefocus.tryspread.model.SynchronizationTask;
 import com.finefocus.tryspread.pojo.*;
 import com.finefocus.tryspread.service.*;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,8 +45,8 @@ public class GetTaskServiceImpl implements GetTaskService {
     private RedisService redisService;
 
     public Map<String, Object> getTask(Integer userId) {
-        Integer taskid = 1;
-        Integer num = 2;
+        Integer taskid = 0;
+        Integer num = 3;
         Map<String, Object> map = new HashMap<String, Object>();
         if (userId == null) {
             map.put(CodeAndMsg.RESULT, CodeAndMsg.ERROR);
@@ -53,41 +54,39 @@ public class GetTaskServiceImpl implements GetTaskService {
             map.put(CodeAndMsg.MSG, "用户id为空");
         }
         if (userId != null) {
-            // 任务与用户建立关联 可加入redis
+            String redisGetTasks = RedisKeyProperties.getPropertyValue("redis_get_tasks");
             List<Task> taskList = null;
             List<Task> newTasks = null;
-            String redisGetTasks = RedisKeyProperties.getPropertyValue("redis_get_tasks");
             try {
                 String taskListRedis = redisService.get(redisGetTasks);
-                if (taskListRedis == null) {
+                if (StringUtils.isEmpty(taskListRedis)) {
                     taskList = taskService.getTasks();
                     redisService.set(redisGetTasks, OBJECT_MAPPER.writeValueAsString(taskList));
                 }
-                if (taskListRedis != null) {
+                if (StringUtils.isNotEmpty(taskListRedis)) {
                     JavaType javaType = JsonTool.getCollectionType(ArrayList.class, Task.class);
                     taskList = OBJECT_MAPPER.readValue(taskListRedis, javaType);
-
-                    for (int i = 0; i < taskList.size(); i++) {
-                        if (taskList.get(i).getId() > taskid) {
-                            newTasks = taskList.subList(i, i + num);
-                            break;
-//                                newTasks.add(taskList.get(i));
-
-                        }
-                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
                 LOGGER.error("从redis中获取全部任务列表失败！！" + e);
             }
-
-//            for (Task task : taskList) {
+            for (int i = 0; i < taskList.size(); i++) {
+                if (taskList.get(i).getId() > taskid) {
+                    newTasks = taskList.subList(i, i + num);
+                    break;
+                }
+            }
+            List<AcquisitionTask> list = new ArrayList<AcquisitionTask>();
             for (Task task : newTasks) {
-                TaskLogBean taskLog = new TaskLogBean();
-                taskLog.setTaskId(task.getId());
-                taskLog.setTaskName(task.getName());
-                taskLog.setUserId(userId);
+                TaskInformationBean taskInformation = taskInfoService.getTaskInfoByTaskId(task.getId());
                 String redisGetStepByTaskId = RedisKeyProperties.getPropertyValue("redis_get_step_by_taskId");
+                Map<String, Object> apkBeanMap = apkService.getApkUrlAndApkIdToMap(task.getId());
+                if (apkBeanMap != null) {
+                    taskInformation.setApkId((Integer) apkBeanMap.get("id"));
+                    taskInformation.setApkUrl((String) apkBeanMap.get("apk_url"));
+                }
+                Integer stepId = taskLogService.getTaskLogByUserIdAndTaskId(userId, task.getId(), 2);
                 List<StepBean> stepList = null;
                 try {
                     String stepListRedis = redisService.get(redisGetStepByTaskId + "_" + task.getId());
@@ -99,55 +98,24 @@ public class GetTaskServiceImpl implements GetTaskService {
                         JavaType javaType = JsonTool.getCollectionType(ArrayList.class, StepBean.class);
                         stepList = OBJECT_MAPPER.readValue(stepListRedis, javaType);
                     }
+                    AcquisitionTask acquisitionTask = new AcquisitionTask();
+                    taskInformation.setTaskId(task.getId());
+                    taskInformation.setTaskName(task.getName());
+                    taskInformation.setTaskType(task.getType());
+                    acquisitionTask.setCurrentStep(stepId);
+                    acquisitionTask.setStep(stepList);
+                    acquisitionTask.setTaskInformation(taskInformation);
+                    list.add(acquisitionTask);
                 } catch (Exception e) {
                     e.printStackTrace();
                     LOGGER.error("根据taskId从redis中获取stepList失败，taskId=" + task.getId(), e);
                 }
-
-                for (StepBean stepBean : stepList) {
-                    taskLog.setStepId(stepBean.getStepId());
-                    taskLog.setTaskIntegration(stepBean.getIntegral());
-                    TaskLogBean taskLogBean = taskLogService.getTaskLogByUserIdAndTaskIdStepId(userId, task.getId(), stepBean.getStepId());
-                    if (taskLogBean == null) {
-                        taskLogService.save(taskLog);
-                    }
-
-                }
-            }
-            LOGGER.info("UserId : " + userId + "初始化任务完成！！");
-            // 返回任务
-            //根据userId查找到所有的task
-            List<Integer> taskIds = taskLogService.getTaskIdsByUserId(userId);
-            List<AcquisitionTask> list = new ArrayList<AcquisitionTask>();
-            for (Integer taskId : taskIds) {
-                //根据taskId获取到下面的所有满足条件的apk的url中的某一个
-                Map<String, Object> apkBeanMap = apkService.getApkUrlAndApkIdToMap(taskId);
-                //根据taskId查找task 与taskInfo 和step
-                Task task = taskService.getTaskByTaskId(taskId);
-                TaskInformationBean taskInformation = taskInfoService.getTaskInfoByTaskId(taskId);
-                if (apkBeanMap != null) {
-                    taskInformation.setApkId((Integer) apkBeanMap.get("id"));
-                    taskInformation.setApkUrl((String) apkBeanMap.get("apk_url"));
-                }
-
-                List<StepBean> steps = stepService.getSteps(taskId);
-                //根据用户id 与taskid  状态为正在做的 2 查找 stepid
-                Integer stepId = taskLogService.getTaskLogByUserIdAndTaskId(userId, taskId, 2);
-                AcquisitionTask acquisitionTask = new AcquisitionTask();
-                taskInformation.setTaskId(taskId);
-                taskInformation.setTaskName(task.getName());
-                taskInformation.setTaskType(task.getType());
-                acquisitionTask.setCurrentStep(stepId);
-                acquisitionTask.setStep(steps);
-                acquisitionTask.setTaskInformation(taskInformation);
-                list.add(acquisitionTask);
             }
             LOGGER.info("UserId : " + userId + "获取任务完成！！");
             map.put(CodeAndMsg.RESULT, CodeAndMsg.SUCCESS);
             map.put(CodeAndMsg.CODE, CodeAndMsg.OK);
             map.put(CodeAndMsg.MSG, "返回结果");
             map.put(CodeAndMsg.DATA, list);
-
         }
         return map;
     }
@@ -158,61 +126,7 @@ public class GetTaskServiceImpl implements GetTaskService {
         TaskLogBean taskLog = taskLogService.getTaskLogByUserIdAndTaskIdStepId(userId, taskId, stepId);
         IntegralBean integralBean = integralService.getIntegralByUserId(userId);
         SynchronizationTask synchronizationTask = new SynchronizationTask();
-        if (taskLog.getState() == 2) {
-            //2代表任务正在进行
-            Date taskCreatTime = taskLog.getTaskCreatTime();//任务创建时间
-            //获取到任务需要执行的之间
-            StepBean stepBean = stepService.getStepByTaskIdStepId(taskId, stepId);
-            Integer useDays = stepBean.getUseDays();
-            //获取第一个任务完成的时间
-            Date taskTimeOk = stepService.getTaskLogTaskTimeByUserIdAndTaskId(userId, taskId);
-            boolean comparisonDate = DateTool.comparisonDate(taskCreatTime, taskTimeOk, useDays);
-            if (comparisonDate) {
-                LOGGER.info("UserId : [" + userId + "]taskId :[ " + taskId + "]stepId :[" + stepId + " ]天数间隔够！");
-                //天数间隔够了
-                //当前时间- 任务创建时间是否满足分钟数要求
-                boolean comparisonMin = DateTool.comparisonMin(taskCreatTime, new Date(), stepBean.getUseTime());
-                if (comparisonMin) {
-                    //分钟数也满足 完成任务
-                    LOGGER.info("UserId : " + userId + "taskId " + taskId + "stepId " + stepId + " 分钟数间隔够！");
-                    taskLogService.completeTask(userId, taskId, stepId, 1, new Date());
-                    //修改积分
-                    Integer integer = stepBean.getIntegral();
-                    integralBean.setTotalIntegral(integralBean.getTotalIntegral() + integer);
-                    integralBean.setCurrentIntegration(integralBean.getCurrentIntegration() + integer);
-                    integralService.updateIntegralById(integralBean);
-
-                    synchronizationTask.setIntegral(integralBean);
-                    synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
-                    map.put(CodeAndMsg.RESULT, CodeAndMsg.SUCCESS);
-                    map.put(CodeAndMsg.CODE, CodeAndMsg.OK);
-                    map.put(CodeAndMsg.MSG, "任务执行完毕");
-                    map.put(CodeAndMsg.DATA, synchronizationTask);
-                    LOGGER.info("UserId : [" + userId + "]taskId : [" + taskId + "] stepId :[ " + stepId + "] 任务执行完毕！");
-                    return map;
-                } else {
-                    synchronizationTask.setIntegral(integralBean);
-                    synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
-                    map.put(CodeAndMsg.RESULT, CodeAndMsg.ERROR);
-                    map.put(CodeAndMsg.CODE, CodeAndMsg.MinutesIsNotEnough);
-                    map.put(CodeAndMsg.MSG, "分钟数不够");
-                    map.put(CodeAndMsg.DATA, synchronizationTask);
-                    LOGGER.info("UserId : [" + userId + "] taskId :[" + taskId + "] stepId :[ " + stepId + " ]分钟间隔不够！");
-                    return map;
-                }
-
-            } else {
-                synchronizationTask.setIntegral(integralBean);
-                synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
-                map.put(CodeAndMsg.RESULT, CodeAndMsg.ERROR);
-                map.put(CodeAndMsg.CODE, CodeAndMsg.DAYIsNotEnough);
-                map.put(CodeAndMsg.MSG, "间隔天数不够");
-                map.put(CodeAndMsg.DATA, synchronizationTask);
-                LOGGER.info("UserId :[ " + userId + "] taskId :[" + taskId + "] stepId :[" + stepId + "] 间隔天数不够！");
-                return map;
-            }
-        }
-        if (taskLog.getState() == 0) {
+        if (taskLog == null) {
             //创建任务
             taskLogService.createTask(userId, taskId, stepId, 2, new Date());
             //返回积分
@@ -225,17 +139,73 @@ public class GetTaskServiceImpl implements GetTaskService {
             LOGGER.info("UserId : [" + userId + "] taskId :[ " + taskId + "] stepId :[ " + stepId + "] 任务创建完成！！");
             return map;
         }
-        if (taskLog.getState() == 1) {
-            //任务已完成，返回积分
-            //返回积分
-            synchronizationTask.setIntegral(integralBean);
-            synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
-            map.put(CodeAndMsg.RESULT, CodeAndMsg.SUCCESS);
-            map.put(CodeAndMsg.CODE, CodeAndMsg.TaskIsOK);
-            map.put(CodeAndMsg.MSG, "该任务已经完成");
-            map.put(CodeAndMsg.DATA, synchronizationTask);
-            LOGGER.info("UserId : [ " + userId + " ] taskId :[" + taskId + "]stepId :[" + stepId + "] 任务已经完成！！");
-            return map;
+        if (taskLog != null) {
+            if (taskLog.getState() == 2) {
+                //2代表任务正在进行
+                Date taskCreatTime = taskLog.getTaskCreatTime();//任务创建时间
+                //获取到任务需要执行的之间
+                StepBean stepBean = stepService.getStepByTaskIdStepId(taskId, stepId);
+                Integer useDays = stepBean.getUseDays();
+                //获取第一个任务完成的时间
+                Date taskTimeOk = stepService.getTaskLogTaskTimeByUserIdAndTaskId(userId, taskId);
+                boolean comparisonDate = DateTool.comparisonDate(taskCreatTime, taskTimeOk, useDays);
+                if (comparisonDate) {
+                    LOGGER.info("UserId : [" + userId + "]taskId :[ " + taskId + "]stepId :[" + stepId + " ]天数间隔够！");
+                    //天数间隔够了
+                    //当前时间- 任务创建时间是否满足分钟数要求
+                    boolean comparisonMin = DateTool.comparisonMin(taskCreatTime, new Date(), stepBean.getUseTime());
+                    if (comparisonMin) {
+                        //分钟数也满足 完成任务
+                        LOGGER.info("UserId : " + userId + "taskId " + taskId + "stepId " + stepId + " 分钟数间隔够！");
+                        taskLogService.completeTask(userId, taskId, stepId, 1, new Date());
+                        //修改积分
+                        Integer integer = stepBean.getIntegral();
+                        integralBean.setTotalIntegral(integralBean.getTotalIntegral() + integer);
+                        integralBean.setCurrentIntegration(integralBean.getCurrentIntegration() + integer);
+                        integralService.updateIntegralById(integralBean);
+
+                        synchronizationTask.setIntegral(integralBean);
+                        synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
+                        map.put(CodeAndMsg.RESULT, CodeAndMsg.SUCCESS);
+                        map.put(CodeAndMsg.CODE, CodeAndMsg.OK);
+                        map.put(CodeAndMsg.MSG, "任务执行完毕");
+                        map.put(CodeAndMsg.DATA, synchronizationTask);
+                        LOGGER.info("UserId : [" + userId + "]taskId : [" + taskId + "] stepId :[ " + stepId + "] 任务执行完毕！");
+                        return map;
+                    } else {
+                        synchronizationTask.setIntegral(integralBean);
+                        synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
+                        map.put(CodeAndMsg.RESULT, CodeAndMsg.ERROR);
+                        map.put(CodeAndMsg.CODE, CodeAndMsg.MinutesIsNotEnough);
+                        map.put(CodeAndMsg.MSG, "分钟数不够");
+                        map.put(CodeAndMsg.DATA, synchronizationTask);
+                        LOGGER.info("UserId : [" + userId + "] taskId :[" + taskId + "] stepId :[ " + stepId + " ]分钟间隔不够！");
+                        return map;
+                    }
+
+                } else {
+                    synchronizationTask.setIntegral(integralBean);
+                    synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
+                    map.put(CodeAndMsg.RESULT, CodeAndMsg.ERROR);
+                    map.put(CodeAndMsg.CODE, CodeAndMsg.DAYIsNotEnough);
+                    map.put(CodeAndMsg.MSG, "间隔天数不够");
+                    map.put(CodeAndMsg.DATA, synchronizationTask);
+                    LOGGER.info("UserId :[ " + userId + "] taskId :[" + taskId + "] stepId :[" + stepId + "] 间隔天数不够！");
+                    return map;
+                }
+            }
+            if (taskLog.getState() == 1) {
+                //任务已完成，返回积分
+                //返回积分
+                synchronizationTask.setIntegral(integralBean);
+                synchronizationTask.setTaskLog(taskLogService.getTaskLogByUserId(userId));
+                map.put(CodeAndMsg.RESULT, CodeAndMsg.SUCCESS);
+                map.put(CodeAndMsg.CODE, CodeAndMsg.TaskIsOK);
+                map.put(CodeAndMsg.MSG, "该任务已经完成");
+                map.put(CodeAndMsg.DATA, synchronizationTask);
+                LOGGER.info("UserId : [ " + userId + " ] taskId :[" + taskId + "]stepId :[" + stepId + "] 任务已经完成！！");
+                return map;
+            }
         }
 
         return map;
